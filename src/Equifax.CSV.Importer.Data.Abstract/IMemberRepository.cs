efe11139085544c5ab4c2b675c5d997e8f3358ab
Damon <damonmorris16@gmail.com>
2019-10-10 18:01:15 +0100

    added 'AddMany' repository method for bulk inserts

--- src/Equifax.CSV.Importer.Data.Abstract/IMemberRepository.cs
+++ src/Equifax.CSV.Importer.Data.Abstract/IMemberRepository.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Equifax.CSV.Importer.Models;
+using System.Collections.Generic;
 
 namespace Equifax.CSV.Importer.Data.Abstract
 {
     public interface IMemberRepository
     {
+        void AddMany(IEnumerable<Member> data);
     }
 }
--- src/Equifax.CSV.Importer.Data.Concrete/MemberRepository.cs
+++ src/Equifax.CSV.Importer.Data.Concrete/MemberRepository.cs
@@ -1,9 +1,50 @@
-﻿using Equifax.CSV.Importer.Data.Abstract;
-using System;
+﻿using Member = Equifax.CSV.Importer.Models.Member;
+using Equifax.CSV.Importer.Data.Abstract;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using FastMember;
 
 namespace Equifax.CSV.Importer.Data.Concrete
 {
     public class MemberRepository : IMemberRepository
     {
+        IConnectionFactory _connectionFactory;
+
+        public MemberRepository(IConnectionFactory connectionFactory)
+        {
+            _connectionFactory = connectionFactory;
+        }
+
+        public void AddMany(IEnumerable<Member> data)
+        {
+            using (var connection = _connectionFactory.CreateLocalConnection())
+            {
+                var bulkCopy = new SqlBulkCopy(connection as SqlConnection,
+                    SqlBulkCopyOptions.TableLock |
+                    SqlBulkCopyOptions.FireTriggers |
+                    SqlBulkCopyOptions.UseInternalTransaction, null);
+
+                bulkCopy.DestinationTableName = "[dbo].[Members]";
+                bulkCopy.BatchSize = data?.Count() ?? 0;
+
+                connection.Open();
+
+                bulkCopy.ColumnMappings.Add("col1", "Id");
+                bulkCopy.ColumnMappings.Add("col2", "Name");
+                bulkCopy.ColumnMappings.Add("col3", "DateOfBirth");
+                bulkCopy.ColumnMappings.Add("col4", "Height");
+                bulkCopy.ColumnMappings.Add("col5", "Balance");
+
+                var records = data.Select(mem => new { col1 = mem.Id, col2 = mem.Name, col3 = mem.DateOfBirth, col4 = mem.Height, col5 = mem.Balance });
+
+                using (var reader = ObjectReader.Create(records, "col1", "col2", "col3", "col4", "col5"))
+                {
+                    bulkCopy.WriteToServer(reader);
+                };
+
+                connection.Close();
+            }
+        }
     }
 }
